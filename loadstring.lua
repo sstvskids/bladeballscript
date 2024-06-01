a9bbe77f7523228bb116610ceafaf19b1bc1abaf
sstvskids <126665568+sstvskids@users.noreply.github.com>
2024-06-01 20:16:42 +1000

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/bladeballscript/main/moreunc.lua",true))()
-task.wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/bladeballscript/main/bladeball.lua",true))()
